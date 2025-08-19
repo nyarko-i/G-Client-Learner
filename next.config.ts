516d3c90ce1b6b28fcbf34d2c853036a366c04ec
nyarko-i <inyarko98@gmail.com>
2025-08-19 15:57:27 +0000

    various setup tools install

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
+import "dotenv/config";
+
+const API_BASE = process.env.NEXT_PUBLIC_API_URL;
+
+if (!API_BASE) {
+  throw new Error("NEXT_PUBLIC_API_URL is not defined in .env.local");
+}
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${API_BASE.replace(/\/$/, "")}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
